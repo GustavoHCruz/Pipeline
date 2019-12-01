9c53d75e9f4f735d774e2ae4edf59ef95aa27119
Gustavo <ghfc2000@hotmail.com>
2019-12-01 11:20:42 -0300

    Começando a fazer o simulador

---
+++ Pipeline.py
@@ -0,0 +1,19 @@
+quantidadeInstrucoes = 5
+
+
+memoria = []
+
+for i in range(quantidadeInstrucoes):
+   memoria.append(input())
+
+for i in range(quantidadeInstrucoes):
+   memoria[i].replace(" ",",")
+   memoria[i].replace(",,",",")
+   
+aux = memoria[0].split(",")
+print("Auxiliar:",aux,"\n\nMemória:",memoria)
+
+x = "ola mano eu sou o dudi"
+x.replace(" ",",")
+
+print(x)
