70d24b61f9a2f61281f8611fc3573c21b933e948
Gustavo <ghfc2000@hotmail.com>
2019-12-01 13:35:49 -0300

    Progresso

--- Pipeline.py
+++ Pipeline.py
@@ -1,19 +1,38 @@
-quantidadeInstrucoes = 5
+# Quantidade de instruções
+inst = 25
+# Tamanho da memória
+mem = 25
+# Quantidade de Registradores
+reg = 10
 
+# Função para inicializar a Memória de Dados
+def iniciar_memoria():
+   memoria = []
+   for i in range(mem):
+      memoria.append(0)
+   return memoria
 
-memoria = []
+# Função para inicializar os valores dos Registradores internos
+def iniciar_registradores():
+   registradores = []
+   for i in range(reg):
+      registradores.append(0)
+   return registradores
 
-for i in range(quantidadeInstrucoes):
-   memoria.append(input())
+# Função para pegar e inicializar a Memória de Instruções
+def iniciar_instrucoes():
+   instrucoes = []
+   for i in range(inst):
+      instrucoes.append(input())
+      instrucoes[i] = instrucoes[i].replace(" ",",")
+      instrucoes[i] = instrucoes[i].replace(",,",",")
+      instrucoes[i] = instrucoes[i].replace("$","")
+      instrucoes[i] = instrucoes[i].replace("\\","")
+      instrucoes[i] = instrucoes[i].replace("[","")
+      instrucoes[i] = instrucoes[i].replace("]","")
+   return instrucoes
 
-for i in range(quantidadeInstrucoes):
-   memoria[i].replace(" ",",")
-   memoria[i].replace(",,",",")
-   
-aux = memoria[0].split(",")
-print("Auxiliar:",aux,"\n\nMemória:",memoria)
-
-x = "ola eu sou o dudi"
-x.replace(" ",",")
-
-print(x)
+# Main
+memoria = iniciar_memoria()
+registradores = iniciar_registradores()
+instrucoes = iniciar_instrucoes()
