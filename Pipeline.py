f6cf94a1b77af428d602c24ef7c8f080b0af3515
Gustavo <ghfc2000@hotmail.com>
2019-12-02 09:19:13 -0300

    Atualizando

--- Pipeline.py
+++ Pipeline.py
@@ -11,11 +11,14 @@ clock = 1
 # Registradores internos
 PC = 0
 IR = []
+IBR = []
 
 # Vetor com registradores em execução
 dep = []
 # Vetor com os comandos do pipeline
-pipeline = []
+pipeline = ["NIL","NIL","NIL","NIL"]
+# Código da operação atual
+opcode = 0
 
 # Função para inicializar a Memória de Dados
 def iniciar_memoria():
@@ -44,32 +47,56 @@ def iniciar_instrucoes():
       instrucoes[i] = instrucoes[i].replace("]","")
    return instrucoes
 
+def UC(IR):
+   a = 0
+
+def ULA(IBR,op):
+   if op == 1: # Soma
+      return = IBR[0] + IBR[1]
+   if op == 2: # Subtração
+      return = IBR[0] - IBR[1]
+   if op == 3: # Copia
+      return = IBR[1]
+
+
 def imprimir():
    print("Ciclo de Clock atual:",clock)
    print("\nMemória de Dados:\n",memoria)
    print("\nMemória de Registradores:\n",registradores)
    print("\nRegistrador Interno:",PC)
-   print("\nPipeline:\nBusca de Instrução:")
+   print("\nPipeline:")
+   print("\nBusca de Instrução:",pipeline[0])
+   print("\nDecoficiação:",pipeline[1])
+   print("\nExecução:",pipeline[2])
+   print("\nEscrita:",pipeline[3])
 
 def busca():
-   IR = instrucoes[PC].split(",")
+   pipeline[0] = instrucoes[PC]
+   IR = instrucoes[PC]
    PC = PC+1
 
 def decodificacao():
-   a = 0
+   pipeline[1] = IR
+   opcode = IBR[0]
+   IBR = IR.split(",")
+   IBR.pop(0)
 
 def execucao():
-   a = 0
+   pipeline[2] = IR
 
 def escrita():
-   a = 0
+   pipeline[3] = IR
 
 def clock():
-   imprimir()
-   clock = clock+1
+   while i < inst:
+      i = i+1
+      imprimir()
+      clock = clock+1
+
 
 # Main
 memoria = iniciar_memoria()
 registradores = iniciar_registradores()
 instrucoes = iniciar_instrucoes()
 
+clock()
