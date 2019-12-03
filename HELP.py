523f0da68d1a01d1e2b337f2ef37a09250239ddb
Gustavo <ghfc2000@hotmail.com>
2019-12-03 19:44:33 -0300

    Progresso sendo feito

--- HELP.py
+++ HELP.py
@@ -3,20 +3,21 @@ mem = 1 # Tamanho da memória de dados
 reg = 2 # Quantidade de registradores de uso geral
 PC = 3 # Program Counter
 IR = 4 # Instruction Register
-IBR = 5 # Instruction Buffer Register
 clock = 6
+pause = [0]
 
-dicionario = {inst: 5,mem: 20,reg: 10,PC: 0,IR: "",IBR: "",clock: 0}
+dicionario = {inst: 14,mem: 20,reg: 10,PC: 0,IR: "",clock: 1}
 memoria = [] # Memória de dados 
 registradores = [] # Lista dos registradores
 instrucoes = [] # Memória de instruções
 
 pipeline = ["NIL","NIL","NIL","NIL"] # Instruções dentro de cada etapa do pipeline
+IBR = ["NIL","NIL","NIL"]
 dep = [""] # Lista de registradores dentro do pipelile
 
 def inicializarMemoria():
    for i in range(dicionario[mem]):
-      memoria.append(0)   
+      memoria.append(0)
 
 def inicializarRegistradores():
    for i in range(dicionario[reg]):
@@ -32,115 +33,130 @@ def busca():
    dicionario[PC] += 1
 
 def decodificacao():
-   dicionario[IBR] = dicionario[IR]
-   dicionario[IBR] = dicionario[IBR].replace(" ",",")
-   dicionario[IBR] = dicionario[IBR].replace(",,",",")
-   dicionario[IBR] = dicionario[IBR].replace("\\","")
-   dicionario[IBR] = dicionario[IBR].replace("$","")
-   dicionario[IBR] = dicionario[IBR].replace("r","")
-   dicionario[IBR] = dicionario[IBR].replace("[","")
-   dicionario[IBR] = dicionario[IBR].replace("]","")
-   dicionario[IBR] = dicionario[IBR].split(",")
+   IBR[0] = dicionario[IR]
+   IBR[0] = IBR[0].replace(" ",",")
+   IBR[0] = IBR[0].replace(",,",",")
+   IBR[0] = IBR[0].replace("\\","")
+   IBR[0] = IBR[0].replace("$","")
+   IBR[0] = IBR[0].replace("r","")
+   IBR[0] = IBR[0].replace("[","")
+   IBR[0] = IBR[0].replace("]","")
+   IBR[0] = IBR[0].split(",")
   
-   dicionario[IBR][1] = int(dicionario[IBR][1])
-   dicionario[IBR][2] = int(dicionario[IBR][2])
-   dicionario[IBR][-1] = int(dicionario[IBR][-1])
+   IBR[0][1] = int(IBR[0][1])
+   IBR[0][2] = int(IBR[0][2])
+   IBR[0][-1] = int(IBR[0][-1])
 
 def execucao():
-   r1 = dicionario[IBR][1]
-   r2 = dicionario[IBR][2]
-   r3 = dicionario[IBR][-1]
+   r1 = IBR[1][1]
+   r2 = IBR[1][2]
+   r3 = IBR[1][-1]
 
    resultado = 0
 
-   if dicionario[IBR][0] == "lw":
+   if IBR[1][0] == "lw":
       resultado = memoria[r3]
-   elif dicionario[IBR][0] == "sw":
+      pause[0] = 1
+   elif IBR[1][0] == "sw":
       resultado = registradores[r1]
-   elif dicionario[IBR][0] == "li":
+      pause[0] = 1
+   elif IBR[1][0] == "li":
       resultado = r3
-   elif dicionario[IBR][0] == "move":
+   elif IBR[1][0] == "move":
       resultado = registradores[r2]
-   elif dicionario[IBR][0] == "add":
+   elif IBR[1][0] == "add":
       resultado = registradores[r2] + registradores[r3]
-   elif dicionario[IBR][0] == "addi":
-      restulado = registradores[r2] + r3
-   elif dicionario[IBR][0] == "sub":
+      pause[0] = 1
+   elif IBR[1][0] == "addi":
+      resultado = registradores[r2] + r3
+      print(resultado)
+   elif IBR[1][0] == "sub":
       resultado = registradores[r2] - registradores[r3]
-   elif dicionario[IBR][0] == "subi":
+      pause[0] = 1
+   elif IBR[1][0] == "subi":
       resultado = registradores[r2] - r3
    
    return resultado
 
 def escrita(resultado):
-   r1 = dicionario[IBR][1]
-   r2 = dicionario[IBR][2]
-   r3 = dicionario[IBR][-1]
+   r1 = IBR[2][1]
+   r2 = IBR[2][2]
+   r3 = IBR[2][-1]
 
-   if dicionario[IBR][0] == "lw":
+   if IBR[2][0] == "lw":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "sw":
+   elif IBR[2][0] == "sw":
       memoria[r2] = resultado
-   elif dicionario[IBR][0]  == "li":
+   elif IBR[2][0]  == "li":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "move":
+   elif IBR[2][0] == "move":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "add":
+   elif IBR[2][0] == "add":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "addi":
+   elif IBR[2][0] == "addi":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "sub":
+   elif IBR[2][0] == "sub":
       registradores[r1] = resultado
-   elif dicionario[IBR][0] == "subi":
+   elif IBR[2][0] == "subi":
       registradores[r1] = resultado
 
 def imprimir():
-   print("\nCiclo de Clock atual:",dicionario[clock])
-   print("IBR:",dicionario[IBR])
-   print("PC:",dicionario[PC])
+   print("\n=========Ciclo de Clock atual:",dicionario[clock],"========")
+   print("PC:",dicionario[PC] - 1)
    print("Memória de Dados:\n",memoria)
    print("Memória de Registradores:\n",registradores)
    print("Busca de Instrução:",pipeline[0])
    print("Decoficiação:",pipeline[1])
    print("Execução:",pipeline[2])
    print("Escrita:",pipeline[3])
+   print("==========================================")
    dicionario[clock] += 1
 
 def simular():
-
-   while (dicionario[PC] < dicionario[inst]) and (instrucoes[dicionario[PC]] != 0):
-
+   saida = 0
+   for i in range(len(instrucoes)):
+      IBR[2] = IBR[1]
       pipeline[3] = pipeline[2]
       if pipeline[3] != "NIL":
          escrita(saida)
       
-      pipeline[2] = pipeline[1]
-      if pipeline[2] != "NIL":
-         saida = execucao()
+      if pause[0] == 0:
+         IBR[1] = IBR[0]
+         pipeline[2] = pipeline[1]
+         if pipeline[2] != "NIL":
+            saida = execucao()
 
-      pipeline[1] = pipeline[0]
-      if pipeline[1] != "NIL":
-         decodificacao()
+         pipeline[1] = pipeline[0]
+         if pipeline[1] != "NIL":
+            decodificacao()
       
-      busca()
-
+         busca()
+      else:
+         pipeline[2] = "NIL"
+         pause[0] = 0
+      
       imprimir()
    
    while (pipeline[3] != "NIL") or (pipeline[2] != "NIL") or (pipeline[1] != "NIL"):
-
+      IBR[2] = IBR[1]
       pipeline[3] = pipeline[2]
       if pipeline[3] != "NIL":
          escrita(saida)
       
-      pipeline[2] = pipeline[1]
-      if pipeline[2] != "NIL":
-         saida = execucao()
+      if pause[0] == 0:
+         IBR[1] = IBR[0]
+         pipeline[2] = pipeline[1]
+         if pipeline[2] != "NIL":
+            saida = execucao()
 
-      pipeline[1] = pipeline[0]
-      if pipeline[1] != "NIL":
-         decodificacao()
-
-      pipeline[0]  = "NIL"
+         pipeline[1] = pipeline[0]
+         if pipeline[1] != "NIL":
+            decodificacao()
+      
+         pipeline[0] = "NIL"
+      else:
+         pipeline[2] = "NIL"
+         pause[0] = 0
 
       imprimir()
 
